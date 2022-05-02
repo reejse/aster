56c148ab57f07c5d63dcb04e3cea4ecae89e7456
reejse <97985577+reejse@users.noreply.github.com>
2022-05-01 23:06:03 -0500

    final

---
+++ assets/other.js
@@ -0,0 +1,32 @@
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+if(navToggle){
+    navToggle.addEventListener('click',() => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+function scrollHeader (){
+    const header = document.getElementById('header')
+    const shop = document.getElementById('cart-shop')
+    const toggle = document.getElementById('nav-toggle')
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
